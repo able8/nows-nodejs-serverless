d2a67ffe28b9e87909a3183def0bd6fa896adf24
a <a@a.com>
2019-11-13 20:35:17 +0800

    refactor: nodejs and serverless

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# 毒鸡汤
-最终我还是开源了，
+# 毒鸡汤 Node.js Serverless 版
 
-实在扛不住哥几个采集数据的，
+源项目 https://github.com/egotong/nows 真是简洁又有趣，只是用的 php+mysql 部署麻烦。
 
-代码真的很丑！
+我用 Node.js 读 txt 代替，放到阿里云函数计算，无需购买服务器，按访问量计费。
 
-没事打开http://www.nows.fun/ 毕竟人生苦短都没苦笑过有什么意思！
+静态资源放 OSS，绑定域名，申请免费 HTTPS 证书，很快就部署好了。
+
+而且阿里云函数计算每月前 100 万次调用免费。
+
+对于简单小项目，Serverless 真是方便又便宜。
+
+部署完成后： 最毒鸡汤 https://3www.top
---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+module.exports.handler = function(req, res, context) {
+    fs.readFile('data.txt', 'utf8', function(err, data) {
+        const lines = data.split('\n');
+        number = Math.floor(Math.random() * lines.length);
+        result = lines[number];
+        // result = result.replace(/，/g, '，<br>');
+        // console.log(result);
+
+        fs.readFile('index.html', 'utf8', function(err, originHtml) {
+            const html = originHtml.replace(/to be replaced/, result);
+            res.setStatusCode(200);
+            res.setHeader('content-type', 'text/html');
+            res.send(html);
+        });
+    });
+};
